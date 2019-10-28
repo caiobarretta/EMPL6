c8b0f2f4307d2224eeafa0e02befb16e29b55709
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-10-27 22:29:44 -0300

    .

--- iot-dashboard/src/app/app.component.ts
+++ iot-dashboard/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'iot-dashboard';
 }
--- iot-dashboard/src/app/app.module.ts
+++ iot-dashboard/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { ChartsModule } from 'ng2-charts';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LineChartComponent } from './dashboard/line-chart/line-chart.component';
+import { PieChartComponent } from './dashboard/pie-chart/pie-chart.component';
+import { BarChartComponent } from './dashboard/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './dashboard/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './dashboard/radar-chart/radar-chart.component';
+import { PolarAreaChartComponent } from './dashboard/polar-area-chart/polar-area-chart.component';
+import { BubbleChartComponent } from './dashboard/bubble-chart/bubble-chart.component';
+import { ScatterChartComponent } from './dashboard/scatter-chart/scatter-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LineChartComponent,
+    PieChartComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PolarAreaChartComponent,
+    BubbleChartComponent,
+    ScatterChartComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
